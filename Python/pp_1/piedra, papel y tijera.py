60f39878ab62445b3c4079ef33d51bbbba4d04e5
Jair Varela <jairjvarela@outlook.com>
2023-07-11 04:34:18 -0600

    Se modifico todo el archivo de piedra papel o tijera, agregando un ciclo completo al juego dependiendo de cuantos partidas se quiera jugar y agregando un sistema de puntuaciones

--- Python/pp_1/piedra, papel y tijera.py
+++ Python/pp_1/piedra, papel y tijera.py
@@ -1,39 +1,62 @@
 import random
+import os
+l = True
 
-options = ('piedra', 'papel', 'tijera')
-print('¡Bienvenido a piedra, papel o tijera!')
-
-user_option = input('¿Qué eliges piedra, papel o tijera? ')
-user_option = user_option.lower()
-while user_option not in options:
-  print('¡Opción no válida!')
-  user_option = input('¿Qué eliges piedra, papel o tijera? ')
-  user_option = user_option.lower()
-
-pc_option = random.choice(options)
-print('El usuario eligió: ' + user_option)
-print('La pc eligió: ' + pc_option)
-
-if user_option == pc_option:
-  print('¡Empate!')
-elif user_option == 'piedra':
-  if pc_option == 'tijera':
-    print('¡Piedra le gana a tijera!')
-    print('¡User win!')
-  else:
-    print('¡Papel le gana a piedra!')
-    print('¡PC win!')
-elif user_option == 'papel':
-  if pc_option == 'piedra':
-    print('¡Papel le gana a piedra!')
-    print('¡User win!')
-  else:
-    print('¡Tijera le gana a papel!')
-    print('¡PC win!')
-elif user_option == 'tijera':
-  if pc_option == 'papel':
-    print('¡Tijera le gana a papel!')
-    print('¡User win!')
-  else:
-    print('¡Piedra le gana a tijera!')
-    print('¡PC win!')
+options = ('piedra 🪨', 'papel 🗞️', 'tijera ✂️')
+def clear():
+    if turnos != 0:
+        os.system('pause')
+    os.system('cls' if os.name == 'nt' else 'clear')
+def tur():
+    sg=True
+    while sg:
+        turnos = input('¿Cuantos turnos quieres jugar? ')
+        if turnos.isdigit() and int(turnos) > 0:
+            sg=False
+        else:
+            print('Ingresa un numero valido mayor a 0')
+    return int(turnos)
+while l:
+    u, c, e, t = 0, 0, 0, 0
+    print('Bienvenido a piedra, papel o tijera')
+    turnos = tur()
+    while turnos > 0:
+        clear()
+        print('1.- Piedra 🪨 | 2.- Papel 🗞️ | 3.- Tijera ✂️')
+        print(f'Puntuación: | Usuario: {u} | PC: {c} | Empates: {e} | Turnos: {turnos} |')
+        pc_option = random.choice(options)
+        user = input('Ingresa tu opción: ')
+        if len(user)>0 and user in options or len(user)>0 and int(user) <= len(options) and int(user)>0:
+            if user.isdigit():
+                user = options[int(user)-1]
+            print(f'PC: {pc_option}  VS Usuario: {user}')
+            if pc_option != user:
+                if (pc_option == 'piedra' and user == 'papel') or (pc_option == 'papel' and user == 'tijera') or (pc_option == 'tijera' and user == 'piedra'):
+                    print('Ganaste')
+                    u += 1
+                    turnos -= 1
+                else:
+                    print('Perdiste')
+                    c += 1
+                    turnos -= 1
+            else:
+                print('Empate')
+                e += 1
+                turnos -= 1
+        else:
+            print('Ingresa una opción valida')
+    clear()
+    print(f'Puntuación: | Usuario: {u} | PC: {c} | Empates: {e} |')
+    if u > c:
+        print('🏆🥇🎉🎊¡Ganaste!🎊🎉🥇🏆')
+    elif u < c:
+        print('💀🤣😂👎Perdiste👎😂🤣💀')
+    else:
+        print('Empate')
+    l = input('¿Quieres volver a jugar? (s/n) ').lower()
+    if l == 's':
+        l = True
+    else:
+        l = False
+clear()
+print('Gracias por jugar')
