c6fdb29a3d4aa3e32f6ad3cd0b1ac34175f00fe0
Jair Varela <jairjvarela@outlook.com>
2023-07-30 00:24:53 -0600

    actualizado un archivo y agregado otro

--- Python/cp2/app/main.py
+++ Python/cp2/app/main.py
@@ -1,10 +1,5 @@
 import utils
 
-keys, values = utils.get_population()
-print(keys)
-print(values)
-print(keys, values)
-
 data = [
     {'Country': 'Colombia', 'Population': 50},
     {'Country': 'Argentina', 'Population': 100},
@@ -18,7 +13,18 @@ data = [
     {'Country': 'Brasil', 'Population': 90},
     {'Country': 'Mexico', 'Population': 5},
 ]
-country = input('Type Country => ')
 
-result = utils.population_by_country(data, country)
-print(result)
+def run():
+    keys, values = utils.get_population()
+    print(keys)
+    print(values)
+    print(keys, values)
+
+    country = input('Type Country => ')
+    country = country.capitalize()
+
+    result = utils.population_by_country(data, country)
+    print(result)
+
+if __name__ == '__main__':
+    run()
